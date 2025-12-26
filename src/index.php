8f900a8190b33e1c46040717718ccbb3a574985a
Marcos Aleixo <maknytz@gmail.com>
2025-12-26 11:12:50 -0300

    Criando a listagem com o switch case

--- src/index.php
+++ src/index.php
@@ -1,7 +1,29 @@
 <?php
 
 require 'Produto.php';
-
 $produto = new Produto();
 
-$produto->list();
+switch ($_GET['operacao']) {
+    case 'list':
+        echo "<h3>Produtos: </h3>";
+        foreach ($produto->list() as $key => $value) {
+            echo "Id: {$value['id']} <br>Descrição: {$value['descricao']} <hr>";
+        }
+        break;
+    
+    case 'insert':
+        
+        break;
+    
+    case 'update':
+        # code...
+        break;
+    
+    case 'delete':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
